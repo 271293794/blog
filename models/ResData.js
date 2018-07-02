04593c25899ea891ba94b3aefa0d027abf77675e
云行 邓 <271293794@qq.com>
2018-07-02 08:27:40 +0800

    添加请求返回数据类的说明。

--- models/ResData.js
+++ models/ResData.js
@@ -1,11 +1,17 @@
-module.exports= function ResData(code=0,msg='',data=null) {
-    this.code=code
-    this.msg=msg
-    this.data=data
+/**
+ * 请求的返回数据类
+ * @param {*返回代码} code 
+ * @param {*消息} msg 
+ * @param {*数据} data 
+ */
+module.exports = function ResData(code = 0, msg = '', data = null) {
+    this.code = code
+    this.msg = msg
+    this.data = data
 
 }
 
- 
-    
+
+
 
 
